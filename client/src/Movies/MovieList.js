da9cb7703aa71d37138655ef917dcab4525425d4
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-04-22 17:12:27 -0600

    Reached MVP and attempted to match styling

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function MovieList({ movies }) {
+  console.log("Movies prop: ", movies)
   return (
     <div className="movie-list">
       {
